230afcd5ece25510d6ea559b320a626c9e394be6
Jin <novhunter@gmail.com>
2023-11-09 15:41:30 -0700

    implemented Integer Power Template Function

--- CS1410_MathLibraryDevelopmentWithTemplates/CS1410_MathLibraryDevelopmentWithTemplates/CS1410_MathLibraryDevelopmentWithTemplates.cpp
+++ CS1410_MathLibraryDevelopmentWithTemplates/CS1410_MathLibraryDevelopmentWithTemplates/CS1410_MathLibraryDevelopmentWithTemplates.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 template<typename T>
-T absolute(T x) {	// Absolute Value Template Function
+T Absolute(T x) {	// Absolute Value Template Function
 	if (x >= 0) {
 		return x;
 	}
@@ -12,20 +12,37 @@ T absolute(T x) {	// Absolute Value Template Function
 	}
 }
 
+double p;
+template<typename T>
+T Power(T x,int n) {	// Integer Power Template Function
+	p = x;
+	for (int i = 0; i < n - 1; i++) {
+		p *= x;
+	}
+	return p;
+}
+
 int main()
 {
 	// Absolute Value
-	cout << "My abs(-1):" << absolute(-1) << endl;
+	cout << "My Absolute(-1):" << Absolute(-1) << endl;
 	cout << "std::abs(-1):" << abs(-1) << endl;
-	cout << "My abs(12):" << absolute(12) << endl;
+	cout << "My Absolute(12):" << Absolute(12) << endl;
 	cout << "std::abs(12):" << abs(12) << endl;
-	cout << "My abs(0):" << absolute(0) << endl;
+	cout << "My Absolute(0):" << Absolute(0) << endl;
 	cout << "std::abs(0):" << abs(0) << endl;
-	cout << "My abs(-3.6):" << absolute(-3.6) << endl;
+	cout << "My Absolute(-3.6):" << Absolute(-3.6) << endl;
 	cout << "std::abs(-3.6):" << abs(-3.6) << endl << endl;
 
 	// Integer Power
-
+	cout << "My Power(2,2):" << Power(2, 2) << endl;
+	cout << "std::pow(2,2):" << pow(2, 2) << endl;
+	cout << "My Power(1.5, 5):" << Power(1.5, 5) << endl;
+	cout << "std::pow(1.5, 5):" << pow(1.5, 5) << endl;
+	cout << "My Power(-4, 6):" << Power(-4, 6) << endl;
+	cout << "std::pow(-4, 6):" << pow(-4, 6) << endl;
+	cout << "My Power(10, 1):" << Power(10, 1) << endl;
+	cout << "std::pow(10, 1):" << pow(10, 1) << endl;
 
 }
 
