70f9670e7cf52eac18b2d968e4deca5f0833bb14
Jin <novhunter@gmail.com>
2023-11-19 10:58:47 -0700

    implemented and formatted leaderboard display

--- CS1410_LeaderboardGame/CS1410_LeaderboardGame/CS1410_LeaderboardGame.cpp
+++ CS1410_LeaderboardGame/CS1410_LeaderboardGame/CS1410_LeaderboardGame.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 using namespace std;
 
-void DisplayLeaderboard();
+void DisplayLeaderboard(vector<string> name, vector<string> score);
 
 string LeaderboardNames = "leaderboard/names.txt";		// file containing the names of the top players
 string LeaderboardScores = "leaderboard/scores.txt";	// file containing the scores of the top players
@@ -13,7 +13,7 @@ string LeaderboardScores = "leaderboard/scores.txt";	// file containing the scor
 int main()
 {
 	// Read leaderboard names line-by-line
-	ifstream NamesIn("leaderboard/names.txt");
+	ifstream NamesIn(LeaderboardNames);
 
 	if (!NamesIn.good()) {	// checks if program is able to access names.txt File
 		cout << "unable to locate leaderboard names";
@@ -21,18 +21,35 @@ int main()
 	}
 
 	string line;
-	vector<string> Leaders;
+	vector<string> LeadersName;
 	while (getline(NamesIn, line)) {
-		Leaders.push_back(line);	// adds the line from names.txt to Leaders vector string
+		LeadersName.push_back(line);	// adds the line from names.txt to LeadersName vector string
 	}
 	NamesIn.close();
 
+	// Read leaderboard scores line-by-line
+	ifstream ScoresIn(LeaderboardScores);
+
+	if (!ScoresIn.good()) {	// checks if program is able to access scores.txt File
+		cout << "unable to locate leaderboard scores";
+		exit(1);	// Returns error code
+	}
+
+	string line1;
+	vector<string> LeadersScore;
+	while (getline(ScoresIn, line1)) {
+		LeadersScore.push_back(line1);	// adds the line from scores.txt to LeadersScore vector string
+	}
+	ScoresIn.close();
+
+	DisplayLeaderboard(LeadersName, LeadersScore);
 }
 
-void DisplayLeaderboard() {	// displays the current leaderboard to the player
+void DisplayLeaderboard(vector<string> name, vector<string> score) {	// displays the current leaderboard to the player
 	cout << "Number Guessing Game Leaderboard" << endl << endl;
-	cout << "Rank | Names  | Guesses" << endl;
+	cout << "  Rank  | Names  | Guesses" << endl;
+	cout << "--------------------------" << endl;
 	for (int i = 1; i < 6; i++) {
-		cout << i << "    | " << (player name) << " | " << (player score) << endl;
+		cout << "   " << i << "    | " << name[i] << " |   " << score[i] << endl;
 	}
 }
