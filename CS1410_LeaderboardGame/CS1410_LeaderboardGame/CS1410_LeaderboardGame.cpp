67a43ad02a92f0376783ec7eeffea1b3494d91bd
Jin <novhunter@gmail.com>
2023-11-19 12:23:18 -0700

    implemented game logic

--- CS1410_LeaderboardGame/CS1410_LeaderboardGame/CS1410_LeaderboardGame.cpp
+++ CS1410_LeaderboardGame/CS1410_LeaderboardGame/CS1410_LeaderboardGame.cpp
@@ -5,10 +5,12 @@
 #include <vector>
 using namespace std;
 
-void DisplayLeaderboard(vector<string> name, vector<string> score);
-
 string LeaderboardNames = "leaderboard/names.txt";		// file containing the names of the top players
 string LeaderboardScores = "leaderboard/scores.txt";	// file containing the scores of the top players
+int guesses;
+
+void DisplayLeaderboard(vector<string> name, vector<string> score);	// function prototype for displaying leaderboard
+bool CoinTossGame(char choice);	// function prototype for handling the game
 
 int main()
 {
@@ -43,13 +45,44 @@ int main()
 	ScoresIn.close();
 
 	DisplayLeaderboard(LeadersName, LeadersScore);
+
+	char choice;
+	bool result;
+	do {
+		cout << "(H)eads or (T)ails? ";
+		cin >> choice;
+		result = CoinTossGame(toupper(choice));
+	} while (result != false);
 }
 
 void DisplayLeaderboard(vector<string> name, vector<string> score) {	// displays the current leaderboard to the player
-	cout << "Number Guessing Game Leaderboard" << endl << endl;
-	cout << "  Rank  | Names  | Guesses" << endl;
-	cout << "--------------------------" << endl;
-	for (int i = 1; i < 6; i++) {
-		cout << "   " << i << "    | " << name[i] << " |   " << score[i] << endl;
+	cout << "  Coin Toss Game Leaderboard  " << endl << endl;
+	cout << "  Rank  |  Names  |  Guesses  " << endl;
+	cout << "------------------------------" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "   " << i + 1 << "    |  " << name[i] << " |    " << score[i] << endl;
+	}
+	cout << endl;
+}
+
+bool CoinTossGame(char choice) {
+	char coin;
+	int face = rand() % 10 + 1;
+
+	if (face <= 5) {
+		coin = 'H';
+	}
+	else if (face > 5) {
+		coin = 'T';
+	}
+
+	if (choice == coin) {
+		guesses++;
+		cout << "Correct!" << endl << endl;
+		return true;
+	}
+	else {
+		cout << "Wrong!" << endl << endl;
+		return false;
 	}
 }
