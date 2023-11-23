43fbe4a994cb0b59fc16b28063ea4c78e5925d00
Jin <novhunter@gmail.com>
2023-11-23 10:15:34 -0700

    finished setting up the start of the game

--- CS1410_Orthello/CS1410_Orthello/CS1410_Orthello.cpp
+++ CS1410_Orthello/CS1410_Orthello/CS1410_Orthello.cpp
@@ -7,11 +7,12 @@ const char blank = char(32);
 char board[8][8]{};	// 8x8 game board
 
 void ResetBoard();	// board reset function prototype
+void PrintBoard();	// board formatting function prototype
 
 int main()
 {
 	ResetBoard();
-
+	PrintBoard();
 }
 
 void ResetBoard() {	// function that sets up the game board at the beginning of a new game
@@ -25,4 +26,28 @@ void ResetBoard() {	// function that sets up the game board at the beginning of 
 	board[3][4] = black;
 	board[4][3] = black;
 	board[4][4] = white;
+}
+
+void PrintBoard() {	// function that displays a formatted version of the game board to the player
+	system("cls");	// Windows command
+
+	for (int row = 0; row < 9; row++) {
+		for (int col = 0; col < 9; col++) {
+			if (row == 0 && col == 0) {	// leaves the upper-left corner empty
+				cout << "  ";
+			}
+			else if (row == 0) {	// adds top row letter labels
+				cout << " " << char(col + 96) << " ";
+			}
+			else {
+				if (col == 0) {	// adds left column number labels
+					cout << row << " ";
+				}
+				else {	// adds styling for grid square
+					cout << "[" << board[row - 1][col - 1] << "]";
+				}
+			}
+		}
+		cout << endl;
+	}
 }
